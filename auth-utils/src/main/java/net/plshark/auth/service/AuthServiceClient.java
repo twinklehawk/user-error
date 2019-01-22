425228bb91caaf18d8da6ef520d9f9066034b7d2
alex <alex@plshark.net>
2019-01-21 23:07:35 -0500

    Change validate to use web request

--- auth-utils/src/main/java/net/plshark/auth/service/AuthServiceClient.java
+++ auth-utils/src/main/java/net/plshark/auth/service/AuthServiceClient.java
@@ -2,7 +2,6 @@ package net.plshark.auth.service;
 
 import java.net.URI;
 import java.util.Objects;
-import com.auth0.jwt.JWTVerifier;
 import net.plshark.auth.model.AccountCredentials;
 import net.plshark.auth.model.AuthToken;
 import net.plshark.auth.model.AuthenticatedUser;
@@ -16,18 +15,15 @@ import reactor.core.publisher.Mono;
 public class AuthServiceClient implements AuthService {
 
     private final WebClient webClient;
-    private final JWTVerifier verifier;
     private final String baseUri;
 
     /**
      * Create a new instance
      * @param webClient the web client to use to make requests
-     * @param verifier the verifier to use to verify tokens, only needs to have the public key set up if using keys
      * @param baseUri the base URI of the auth server
      */
-    public AuthServiceClient(WebClient webClient, JWTVerifier verifier, String baseUri) {
+    public AuthServiceClient(WebClient webClient, String baseUri) {
         this.webClient = Objects.requireNonNull(webClient);
-        this.verifier = Objects.requireNonNull(verifier);
         this.baseUri = Objects.requireNonNull(baseUri);
     }
 
@@ -38,8 +34,8 @@ public class AuthServiceClient implements AuthService {
                 .contentType(MediaType.APPLICATION_JSON)
                 .syncBody(credentials)
                 .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .flatMap(response -> response.bodyToMono(AuthToken.class));
+                .retrieve()
+                .bodyToMono(AuthToken.class);
     }
 
     @Override
@@ -49,15 +45,18 @@ public class AuthServiceClient implements AuthService {
                 .contentType(MediaType.TEXT_PLAIN)
                 .syncBody(refreshToken)
                 .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .flatMap(response -> response.bodyToMono(AuthToken.class));
+                .retrieve()
+                .bodyToMono(AuthToken.class);
     }
 
     @Override
     public Mono<AuthenticatedUser> validateToken(String accessToken) {
-        return Mono.defer(() -> {
-            verifier.verify(accessToken);
-            return Mono.empty();
-        });
+        return webClient.post()
+                .uri(URI.create(baseUri + "/auth/validate"))
+                .contentType(MediaType.TEXT_PLAIN)
+                .syncBody(accessToken)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(AuthenticatedUser.class);
     }
 }
