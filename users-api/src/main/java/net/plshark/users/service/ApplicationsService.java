ed1b07d537cf8b577ec308221f8b91a2b4264a3b
alex <alex@plshark.net>
2019-10-30 21:57:18 -0400

    Create controller for managing applications

---
+++ users-api/src/main/java/net/plshark/users/service/ApplicationsService.java
@@ -0,0 +1,40 @@
+package net.plshark.users.service;
+
+import net.plshark.users.model.Application;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Service for managing applications
+ */
+public interface ApplicationsService {
+
+    /**
+     * Retrieve an application by name
+     * @param name the application name
+     * @return the matching application or empty if not found
+     */
+    Mono<Application> get(String name);
+
+    /**
+     * Get all applications up to the maximum result count and starting at an offset
+     * @param maxResults the maximum number of results to return
+     * @param offset the offset to start the list at, 0 to start at the beginning
+     * @return the applications
+     */
+    Flux<Application> getApplications(int maxResults, long offset);
+
+    /**
+     * Save a new application
+     * @param application the application
+     * @return the saved application
+     */
+    Mono<Application> insert(Application application);
+
+    /**
+     * Delete an application
+     * @param name the application name
+     * @return an empty result
+     */
+    Mono<Void> delete(String name);
+}
