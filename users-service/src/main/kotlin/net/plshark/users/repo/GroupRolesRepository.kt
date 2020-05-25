11d19058eb647d9bd261cce7cac4cb7311c40f27
Alex Daidone <alex@plshark.net>
2020-05-25 09:49:57 -0400

    Add docs for GroupRolesRepository.kt

--- users-service/src/main/kotlin/net/plshark/users/repo/GroupRolesRepository.kt
+++ users-service/src/main/kotlin/net/plshark/users/repo/GroupRolesRepository.kt
@@ -4,11 +4,44 @@ import net.plshark.users.model.Role
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
-// TODO docs
+/**
+ * Repository for group-role associations
+ */
 interface GroupRolesRepository {
+    /**
+     * Get roles belonging to a group
+     * @param groupId the ID of the group
+     * @returns all the roles belonging to the group
+     */
     fun getRolesForGroup(groupId: Long): Flux<Role>
+
+    /**
+     * Add a new group-role association
+     * @param groupId the group ID
+     * @param roleId the role ID
+     * @returns an empty Mono to indicate completion
+     */
     fun insert(groupId: Long, roleId: Long): Mono<Void>
+
+    /**
+     * Remove a new group-role association
+     * @param groupId the group ID
+     * @param roleId the role ID
+     * @returns an empty Mono to indicate completion
+     */
     fun delete(groupId: Long, roleId: Long): Mono<Void>
+
+    /**
+    * Remove all associations for a group
+    * @param groupId the group ID
+    * @returns an empty Mono to indicate completion
+    */
     fun deleteForGroup(groupId: Long): Mono<Void>
+
+    /**
+     * Remove all associations for a role
+     * @param roleId the role ID
+     * @returns an empty Mono to indicate completion
+     */
     fun deleteForRole(roleId: Long): Mono<Void>
 }
