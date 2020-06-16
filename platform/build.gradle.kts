8c11287f107e7f442716867292dfd926b9c1dceb
Alex Daidone <alex@plshark.net>
2020-06-16 14:27:50 -0400

    Update java-jwt from 3.10.1 to 3.10.3

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     api(enforcedPlatform("org.springframework.boot:spring-boot-dependencies:2.3.1.RELEASE"))
 
     constraints {
-        api("com.auth0:java-jwt:3.10.1")
+        api("com.auth0:java-jwt:3.10.3")
         api("io.mockk:mockk:1.9.3")
         api("com.google.guava:guava:28.2-jre")
     }
