4237b1baf8b0bf18f200dc5480255a798afba009
alex <alex@plshark.net>
2020-07-15 09:59:12 -0400

    Stop using wildcard imports

--- users-service/src/main/kotlin/net/plshark/users/webservice/UsersController.kt
+++ users-service/src/main/kotlin/net/plshark/users/webservice/UsersController.kt
@@ -8,7 +8,14 @@ import net.plshark.users.model.User
 import net.plshark.users.model.UserCreate
 import net.plshark.users.service.UsersService
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import javax.validation.constraints.Min
