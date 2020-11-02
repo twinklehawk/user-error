dde698de8bb672853a4d7914858c8eb35187bcea
alex <alex@plshark.net>
2020-11-01 21:10:41 -0500

    Remove UsersServiceImpl

--- user-error-server/src/main/kotlin/net/plshark/users/webservice/UsersController.kt
+++ user-error-server/src/main/kotlin/net/plshark/users/webservice/UsersController.kt
@@ -1,14 +1,21 @@
 package net.plshark.users.webservice
 
 import net.plshark.errors.BadRequestException
+import net.plshark.errors.DuplicateException
 import net.plshark.errors.ObjectNotFoundException
 import net.plshark.users.model.Group
 import net.plshark.users.model.PasswordChangeRequest
 import net.plshark.users.model.Role
 import net.plshark.users.model.User
 import net.plshark.users.model.UserCreate
-import net.plshark.users.service.UsersService
+import net.plshark.users.repo.UserGroupsRepository
+import net.plshark.users.repo.UserRolesRepository
+import net.plshark.users.repo.UsersRepository
+import org.springframework.dao.DataIntegrityViolationException
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.http.MediaType
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -20,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 import javax.validation.constraints.Min
 
 /**
@@ -27,86 +35,105 @@ import javax.validation.constraints.Min
  */
 @RestController
 @RequestMapping("/users")
-class UsersController(private val usersService: UsersService) {
+class UsersController(
+    private val userRepo: UsersRepository,
+    private val userRolesRepo: UserRolesRepository,
+    private val userGroupsRepo: UserGroupsRepository,
+    private val passwordEncoder: PasswordEncoder
+) {
 
-    /**
-     * Get all users up to the maximum result count and starting at an offset
-     * @param maxResults the maximum number of results to return
-     * @param offset the offset to start the list at
-     * @return the users
-     */
     @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getUsers(
         @RequestParam(value = "max-results", defaultValue = "50") maxResults: @Min(1) Int,
         @RequestParam(value = "offset", defaultValue = "0") offset: @Min(0) Long
     ): Flux<User> {
-        return usersService.getUsers(maxResults, offset)
+        return userRepo.getAll(maxResults, offset)
     }
 
-    /**
-     * Retrieve a user by ID
-     * @param id the user ID
-     * @return the matching user
-     */
     @GetMapping(path = ["/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun findById(@PathVariable("id") id: Long): Mono<User> {
-        return usersService.findById(id)
+        return userRepo.findById(id)
             .switchIfEmpty(Mono.error { ObjectNotFoundException("No user found for id") })
     }
 
-    /**
-     * Insert a new user
-     * @param user the user to insert
-     * @return the inserted user
-     */
     @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun create(@RequestBody user: UserCreate): Mono<User> {
         if (user.password.isEmpty()) throw BadRequestException("Password cannot be empty")
-        return usersService.create(user)
+        return Mono.just(user)
+            .subscribeOn(Schedulers.parallel())
+            .map { it.copy(password = passwordEncoder.encode(it.password)) }
+            .flatMap { userRepo.insert(it) }
+            .onErrorMap(DataIntegrityViolationException::class.java) { e: DataIntegrityViolationException? ->
+                DuplicateException("A user with username ${user.username} already exists", e)
+            }
     }
 
-    /**
-     * Delete a user by ID
-     * @param id the ID
-     * @return an empty result
-     */
     @DeleteMapping("/{id}")
     fun delete(@PathVariable("id") id: Long): Mono<Void> {
-        return usersService.deleteById(id)
+        return userRepo.deleteById(id)
     }
 
-    /**
-     * Change a user's password
-     * @param id the user ID
-     * @param request the password change request
-     * @return an empty result or ObjectNotFoundException if the user was not found or the current password was
-     * incorrect
-     */
     @PostMapping(path = ["/{id}/password"])
     fun changePassword(
         @PathVariable("id") id: Long,
         @RequestBody request: PasswordChangeRequest
     ): Mono<Void> {
-        return usersService.updateUserPassword(id, request.currentPassword, request.newPassword)
+        if (request.newPassword.isEmpty()) throw BadRequestException("New password cannot be empty")
+        val newPasswordEncoded = passwordEncoder.encode(request.newPassword)
+        val currentPasswordEncoded = passwordEncoder.encode(request.currentPassword)
+        return findRequiredById(id)
+            .flatMap { user: User ->
+                userRepo.updatePassword(user.id, currentPasswordEncoded, newPasswordEncoded)
+                    .onErrorResume(EmptyResultDataAccessException::class.java) { e: EmptyResultDataAccessException? ->
+                        Mono.error(BadRequestException("Incorrect current password", e))
+                    }
+            }
     }
 
     @GetMapping(path = ["/{id}/roles"])
     fun getUserRoles(@PathVariable("id") id: Long): Flux<Role> {
-        return usersService.getUserRoles(id)
+        return userRolesRepo.findRolesByUserId(id)
     }
 
     @PutMapping(path = ["/{id}/roles"])
+    @Transactional
     fun updateUserRoles(@PathVariable("id") id: Long, @RequestBody updatedRoles: Set<Role>): Flux<Role> {
-        return usersService.updateUserRoles(id, updatedRoles)
+        return findRequiredById(id)
+            .thenMany(userRolesRepo.findRolesByUserId(id))
+            .collectList()
+            .map { existingRoles ->
+                Flux.merge(
+                    Flux.fromIterable(existingRoles.minus(updatedRoles))
+                        .flatMap { userRolesRepo.deleteById(id, it.id) },
+                    Flux.fromIterable(updatedRoles.minus(existingRoles))
+                        .flatMap { userRolesRepo.insert(id, it.id) }
+                )
+            }.thenMany(Flux.fromIterable(updatedRoles))
     }
 
     @GetMapping(path = ["/{id}/groups"])
     fun getUserGroups(@PathVariable("id") id: Long): Flux<Group> {
-        return usersService.getUserGroups(id)
+        return userGroupsRepo.findGroupsByUserId(id)
     }
 
     @PutMapping(path = ["/{id}/groups"])
+    @Transactional
     fun updateUserGroups(@PathVariable("id") id: Long, @RequestBody updatedGroups: Set<Group>): Flux<Group> {
-        return usersService.updateUserGroups(id, updatedGroups)
+        return findRequiredById(id)
+            .thenMany(userGroupsRepo.findGroupsByUserId(id))
+            .collectList()
+            .map { existingGroups ->
+                Flux.merge(
+                    Flux.fromIterable(existingGroups.minus(updatedGroups))
+                        .flatMap { userGroupsRepo.deleteById(id, it.id) },
+                    Flux.fromIterable(updatedGroups.minus(existingGroups))
+                        .flatMap { userGroupsRepo.insert(id, it.id) }
+                )
+            }.thenMany(Flux.fromIterable(updatedGroups))
+    }
+
+    private fun findRequiredById(id: Long): Mono<User> {
+        return findById(id)
+            .switchIfEmpty(Mono.error { ObjectNotFoundException("No user found for $id") })
     }
 }
--- user-error-server/src/test/kotlin/net/plshark/users/webservice/UsersControllerTest.kt
+++ user-error-server/src/test/kotlin/net/plshark/users/webservice/UsersControllerTest.kt
@@ -3,78 +3,45 @@ package net.plshark.users.webservice
 import io.mockk.every
 import io.mockk.mockk
 import net.plshark.errors.BadRequestException
+import net.plshark.errors.DuplicateException
+import net.plshark.errors.ObjectNotFoundException
 import net.plshark.users.model.Group
 import net.plshark.users.model.PasswordChangeRequest
 import net.plshark.users.model.Role
 import net.plshark.users.model.User
 import net.plshark.users.model.UserCreate
-import net.plshark.users.service.UsersService
+import net.plshark.users.repo.UserGroupsRepository
+import net.plshark.users.repo.UserRolesRepository
+import net.plshark.users.repo.UsersRepository
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.springframework.dao.DataIntegrityViolationException
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.security.crypto.password.PasswordEncoder
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 import reactor.kotlin.test.test
 import reactor.test.StepVerifier
 import reactor.test.publisher.PublisherProbe
 
+@Suppress("ReactiveStreamsUnusedPublisher")
 class UsersControllerTest {
 
-    private val service = mockk<UsersService>()
-    private val controller = UsersController(service)
+    private val userRepo = mockk<UsersRepository>()
+    private val userRolesRepo = mockk<UserRolesRepository>()
+    private val userGroupsRepo = mockk<UserGroupsRepository>()
+    private val encoder = mockk<PasswordEncoder>()
+    private val controller = UsersController(userRepo, userRolesRepo, userGroupsRepo, encoder)
 
     @Test
-    fun `delete passes the user ID through to be deleted`() {
-        val probe = PublisherProbe.empty<Void>()
-        every { service.deleteById(1) } returns probe.mono()
+    fun `new users have password encoded`() {
+        every { encoder.encode("pass") } returns "pass-encoded"
+        every { userRepo.insert(UserCreate("user", "pass-encoded")) } returns
+                Mono.just(User(1, "user"))
 
-        StepVerifier.create(controller.delete(1))
-            .verifyComplete()
-        probe.assertWasSubscribed()
-        probe.assertWasRequested()
-        probe.assertWasNotCancelled()
-    }
-
-    @Test
-    fun `change password passes the user ID, current password, and new passwords through`() {
-        val probe = PublisherProbe.empty<Void>()
-        every { service.updateUserPassword(2, "current", "new") } returns probe.mono()
-
-        StepVerifier.create(controller.changePassword(2, PasswordChangeRequest("current", "new")))
-            .verifyComplete()
-        probe.assertWasSubscribed()
-        probe.assertWasRequested()
-        probe.assertWasNotCancelled()
-    }
-
-    @Test
-    fun `getUsers passes the max results and offset through`() {
-        val user1 = User(1, "user")
-        val user2 = User(2, "user2")
-        every { service.getUsers(3, 2) } returns Flux.just(user1, user2)
-
-        StepVerifier.create(controller.getUsers(3, 2))
-            .expectNext(user1, user2)
-            .verifyComplete()
-    }
-
-    @Test
-    fun `getUser passes the username through`() {
-        val user1 = User(1, "user")
-        every { service.findById(7) } returns Mono.just(user1)
-
-        StepVerifier.create(controller.findById(7))
-            .expectNext(user1)
-            .verifyComplete()
-    }
-
-    @Test
-    fun `insert passes through the response from the service`() {
-        val request = UserCreate("user", "test-pass")
-        val created = User(1, "user")
-        every { service.create(request) } returns Mono.just(created)
-
-        StepVerifier.create(controller.create(request))
-            .expectNext(created)
+        StepVerifier.create(controller.create(UserCreate("user", "pass")))
+            .expectNext(User(1, "user"))
             .verifyComplete()
     }
 
@@ -86,12 +53,97 @@ class UsersControllerTest {
     }
 
     @Test
-    fun `getUserRoles passes through`() {
+    fun `create should map the exception for a duplicate username to a DuplicateException`() {
+        val request = UserCreate("app", "pass")
+        every { encoder.encode("pass") } returns "pass"
+        every { userRepo.insert(request) } returns Mono.error(DataIntegrityViolationException("test error"))
+
+        StepVerifier.create(controller.create(request))
+            .verifyError(DuplicateException::class.java)
+    }
+
+    @Test
+    fun `cannot update a user to have an empty password`() {
+        assertThrows<BadRequestException> { controller.changePassword(123, PasswordChangeRequest("current", "")) }
+    }
+
+    @Test
+    fun `new password is encoded when updating password`() {
+        every { encoder.encode("current") } returns "current-encoded"
+        every { encoder.encode("new-pass") } returns "new-pass-encoded"
+        every { userRepo.findById(100) } returns Mono.just(User(100, "test"))
+        val probe = PublisherProbe.empty<Void>()
+        every { userRepo.updatePassword(100, "current-encoded", "new-pass-encoded") } returns probe.mono()
+
+        StepVerifier.create(controller.changePassword(100, PasswordChangeRequest("current", "new-pass")))
+            .verifyComplete()
+        probe.assertWasSubscribed()
+        probe.assertWasRequested()
+        probe.assertWasNotCancelled()
+    }
+
+    @Test
+    fun `no matching user when updating password throws exception`() {
+        every { encoder.encode("current") } returns "current-encoded"
+        every { encoder.encode("new") } returns "new-encoded"
+        every { userRepo.findById(200) } returns Mono.empty()
+
+        StepVerifier.create(controller.changePassword(200, PasswordChangeRequest("current", "new")))
+            .verifyError(ObjectNotFoundException::class.java)
+    }
+
+    @Test
+    fun `no matching existing password when updating password throws exception`() {
+        every { encoder.encode("current") } returns "current-encoded"
+        every { encoder.encode("new") } returns "new-encoded"
+        every { userRepo.findById(100) } returns Mono.just(User(100, "ted"))
+        every { userRepo.updatePassword(100, "current-encoded", "new-encoded") } returns Mono.error(
+            EmptyResultDataAccessException(1)
+        )
+
+        StepVerifier.create(controller.changePassword(100, PasswordChangeRequest("current", "new")))
+            .verifyError(BadRequestException::class.java)
+    }
+
+    @Test
+    fun `the user is removed when the user is deleted`() {
+        val userProbe = PublisherProbe.empty<Void>()
+        every { userRepo.deleteById(100) } returns userProbe.mono()
+
+        StepVerifier.create(controller.delete(100))
+            .verifyComplete()
+        userProbe.assertWasSubscribed()
+    }
+
+    @Test
+    fun `deleting by username deletes the user`() {
+        val userProbe = PublisherProbe.empty<Void>()
+        every { userRepo.deleteById(100) } returns userProbe.mono()
+
+        StepVerifier.create(controller.delete(100))
+            .verifyComplete()
+        userProbe.assertWasSubscribed()
+    }
+
+    @Test
+    fun `getUsers should return all results`() {
+        val user1 = User(1, "user")
+        val user2 = User(2, "user2")
+        every { userRepo.getAll(5, 0) } returns Flux.just(user1, user2)
+
+        StepVerifier.create(controller.getUsers(5, 0))
+            .expectNext(user1, user2)
+            .verifyComplete()
+    }
+
+    @Test
+    fun `getUserRoles should return all roles`() {
         val r1 = Role(1, 2, "role1")
         val r2 = Role(2, 2, "role2")
-        every { service.getUserRoles(123) } returns Flux.just(r1, r2)
+        every { userRepo.findById(3) } returns User(3, "test-user").toMono()
+        every { userRolesRepo.findRolesByUserId(3) } returns Flux.just(r1, r2)
 
-        controller.getUserRoles(123).test()
+        controller.getUserRoles(3).test()
             .expectNext(r1, r2)
             .verifyComplete()
     }
@@ -100,10 +152,14 @@ class UsersControllerTest {
     fun `updateUserRoles passes through`() {
         val r1 = Role(1, 2, "role1")
         val r2 = Role(2, 2, "role2")
-        every { service.updateUserRoles(123, setOf(r1, r2)) } returns Flux.just(r1, r2)
+        val r3 = Role(3, 1, "role3")
+        every { userRepo.findById(123) } returns User(123, "test-user").toMono()
+        every { userRolesRepo.findRolesByUserId(123) } returns Flux.just(r1, r2)
+        every { userRolesRepo.deleteById(123, 2) } returns Mono.empty()
+        every { userRolesRepo.insert(123, 3) } returns Mono.empty()
 
-        controller.updateUserRoles(123, setOf(r1, r2)).test()
-            .expectNext(r1, r2)
+        controller.updateUserRoles(123, setOf(r1, r3)).test()
+            .expectNext(r1, r3)
             .verifyComplete()
     }
 
@@ -111,9 +167,10 @@ class UsersControllerTest {
     fun `getUserGroups passes through`() {
         val g1 = Group(1, "group1")
         val g2 = Group(2, "group2")
-        every { service.getUserGroups(321) } returns Flux.just(g1, g2)
+        every { userRepo.findById(4) } returns User(4, "test-user").toMono()
+        every { userGroupsRepo.findGroupsByUserId(4) } returns Flux.just(g1, g2)
 
-        controller.getUserGroups(321).test()
+        controller.getUserGroups(4).test()
             .expectNext(g1, g2)
             .verifyComplete()
     }
@@ -122,10 +179,14 @@ class UsersControllerTest {
     fun `updateUserGroups passes through`() {
         val g1 = Group(1, "group1")
         val g2 = Group(2, "group2")
-        every { service.updateUserGroups(421, setOf(g1, g2)) } returns Flux.just(g1, g2)
+        val g3 = Group(3, "group3")
+        every { userRepo.findById(123) } returns User(123, "test-user").toMono()
+        every { userGroupsRepo.findGroupsByUserId(123) } returns Flux.just(g1, g2)
+        every { userGroupsRepo.deleteById(123, 2) } returns Mono.empty()
+        every { userGroupsRepo.insert(123, 3) } returns Mono.empty()
 
-        controller.updateUserGroups(421, setOf(g1, g2)).test()
-            .expectNext(g1, g2)
+        controller.updateUserGroups(123, setOf(g1, g3)).test()
+            .expectNext(g1, g3)
             .verifyComplete()
     }
 }
