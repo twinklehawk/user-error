589ee6c118cb3e53474c619dd5183d2763ab7545
alex <alex@plshark.net>
2020-11-01 21:22:56 -0500

    Update spring boot to 2.3.5.RELEASE

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -7,7 +7,7 @@ javaPlatform {
 }
 
 dependencies {
-    api(enforcedPlatform("org.springframework.boot:spring-boot-dependencies:2.3.3.RELEASE"))
+    api(enforcedPlatform("org.springframework.boot:spring-boot-dependencies:2.3.5.RELEASE"))
 
     constraints {
         api("com.auth0:java-jwt:3.10.3")
