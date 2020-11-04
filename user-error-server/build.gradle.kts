619dcc81172521c611bca400f11716b3145ce3db
Alex Daidone <alex@plshark.net>
2020-11-03 21:49:37 -0500

    Add coroutines libraries to dependencies

--- user-error-server/build.gradle.kts
+++ user-error-server/build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation(kotlin("reflect"))
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("io.mockk:mockk")
